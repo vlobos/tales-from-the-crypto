da65da8962e1a5e7b1865608def4e1a03118bdc0
vlobos <suryvlobos@gmail.com>
2018-12-08 09:39:22 -0800

    server set up

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
+const server = express();
+const port = 3000;
 
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended: true}));
+
+server.use(express.static(path.join(__dirname, "./public")));
+
+//TODO: set routes
+
+server.listen(3000, () => {
+  console.log("LISTENING TO PORT: ", port);
+})
